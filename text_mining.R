f48e7ce033b681a2027273d9021dc70976e7c388
Riccardo Cervero <48525793+RCrvro@users.noreply.github.com>
2019-08-09 20:17:27 +0200

    Update text_mining.R

--- text_mining.R
+++ text_mining.R
@@ -4,6 +4,8 @@ library(caret)
 library(purrr)
 library(tibble)
 library(dplyr)
+
+## Sampling con doppia stratificazione (in base a 'section' e 'publication')
 db <- read.csv("/Users/riccardocervero/Desktop/news_sample.csv")
 db.train <- read.csv("/Users/riccardocervero/Desktop/train1.csv")
 db.test <- anti_join(db,db.train)
@@ -12,17 +14,17 @@ db <- db[,VAR]
 db.train <- db.train[,VAR]
 db.test <- db.test[,VAR]
 n <- nrow(db)
-## shuffle delle righe
+## Shuffle delle righe
 db.train <- db.train[sample(nrow(db.train),replace = F),]
 db.test <- db.test[sample(nrow(db.test),replace = F),]
 dim(db.train)
 View(db)
 View(db.train)
-## filtra il dataset in base alla categoria
+## Filtra il dataset in base alla categoria
 categories <- unique(db$section)
 get_per_category <- function(category, var)
   db.train[db.train$section == category, var, drop = TRUE]
-## restituisce la matrice della frequenza delle parole all'interno dei
+## Restituisce la matrice della frequenza delle parole all'interno dei
 ## dati, escludendo le parole con frequenza < 2
 get_matrix_words <- function(data, min_freq=2) {
   tdm <- as.matrix(TermDocumentMatrix(Corpus(VectorSource(data)),
@@ -34,7 +36,7 @@ get_matrix_words <- function(data, min_freq=2) {
   names(tdm.sums) <- rownames(tdm)
   return(tdm.sums[which(tdm.sums >= min_freq)])
 }
-## al modello si passa una matrice, non una lista
+## Trasformazione della lista in matrice, da passare al modello
 list_to_dataframe <- function(l) {
   words <- unique(names(flatten(l)))
   out <- map_dfc(names(l), function(category) {
@@ -49,7 +51,8 @@ list_to_dataframe <- function(l) {
   out[is.nan(out)] <- 0
   return(out)
 }
-## combina tutte le funzioni precedenti per ottenere i dati
+## Combinazione di tutte le funzioni precedenti per ottenere i dati
+## su cui allenare i modelli
 get_matrix <- function(var, data) {
   txts <- map(categories, ~get_per_category(.x, var))
   names(txts) <- categories
@@ -59,36 +62,35 @@ get_matrix <- function(var, data) {
   
   return(list_to_dataframe(matrix_per_var))
 }
-## le matrici headlines_matrix <- get_matrix("title", db.train)
-## e content_matrix <- get_matrix("content", db.train)
-## son troppo pesanti per poter essere allocate normalmente, per cui sono
-## richiamate più avanti in modo tale da evitare allocamenti inutili in RAM
+## Le matrici headlines_matrix e content_matrix son troppo pesanti per poter 
+## essere allocate normalmente, per cui sono richiamate più avanti in modo tale
+## da evitare allocamenti inutili in RAM.
 
-## probabilità a priori della distribuzione uniforme
+## Probabilità a priori della distribuzione uniforme
 prior <- 1 / length(categories)
-## costruiamo il modello (per una sola categoria)
+## Costruiamo il modello (per una sola categoria)
 bayes.model <- function(prob, data.train, category, min_prob=1e-20) {
-  ## training: la probabilità è "deformata" in base alla probabilità
-  ## data dalle osservazioni, calcolata però con un calcolo matriciale
+  ## Nella fase di training, la probabilità è "deformata" in base alla probabilità
+  ## data dalle osservazioni, ottenuta con un calcolo matriciale
   ## per risparmiare tempo
   words <- names(data.train[data.train[, category] > 0, category])
   probs <- c(prior * (data.train[words, category] /
                         data.train[words, ] %*% rep(1, ncol(data.train))))
   names(probs) <- words
-  ## restituisce la funzione di previsione
+  ## Restituisce la funzione di previsione
   predict <- function(w) {
     ifelse(probs[w] == 0 || is.na(probs[w]), min_prob, probs[w])
   }
 }
-## costruisce il predittore 
+## Costruisce il predittore 
 predictor <- function(prior, data) {
-  ## costruisce un modello per ogni categoria usando gli stessi dati
+  ## Costruisce un modello per ogni categoria usando gli stessi dati
   predictor.categories <- map(categories,
                               ~bayes.model(prior, data, .x))
   names(predictor.categories) <- categories
-  ## ritorna la funzione di previsione
+  ## Restituisce la funzione di previsione
   predict <- function(txt) {
-    ## il testo è esploso
+    ## Il testo viene esploso
     txt.splitted <- strsplit(as.character(txt), " ")[[1]]
     results <- rep(prior, length(predictor.categories))
     names(results) <- names(predictor.categories)
@@ -105,20 +107,19 @@ predictor <- function(prior, data) {
 ## per tipo (titolo o contenuto) usa dei predittori per singola
 ## categoria (i tipi di articolo) isolati tra di loro
 
-## semplicemente prende la classe più probabile
+## Infine, prende la classe più probabile
 predict.obs <- function(predictor, txt)
   names(which.max(predictor(txt)))
 
-## come sopra, ma effettua in batch per un vettore
+## Come appena effettuato, ma in batch per un vettore
 predict.vector <- function(predictor, v)
   sapply(v, function(x) predict.obs(predictor, x))
 
-## costruiamo e alleniamo i predittori
+## Costruiamo e alleniamo i predittori
 headlines.predictor <- predictor(prior, get_matrix("title", db.train))
 content.predictor <- predictor(prior, get_matrix("content", db.train))
 
-## parte per l'analisi delle prestazioni del classificatore, si può
-## eliminare tranquillamente in futuro
+## Analisi delle prestazioni del classificatore
 results.headline <- mean(predict.vector(headlines.predictor,
                                         db.test$title)
                          == db.test$section)
@@ -128,15 +129,13 @@ results.content <- mean(predict.vector(content.predictor,
 results.headline
 results.content
 
-## parte di Machine Learning puro
-## input: the row dataset from the .csv file
-## output: the dataset ready for the ML algorithm
+##Machine Learning (considerando una doppia stratificazione del training set)
 ## Sarebbe stato necessario inserire la matrice con le probabilità grezze per
 ## ogni categoria e non solamente la categoria più probabile, ma si è scelto di
 ## non farlo per non appesantire ulteriormente la complessità computazionale 
-##dell'algoritmo, che non potrebbe comunque dare particolari risultati,
-##perché le probabilità tendono quasi sempre a 0 o 1, evitando la maledizione della dimensionalità.
-## Basta dunque solamente mettere qualcosa per bilanciare titolo e contenuto.
+## dell'algoritmo, che non potrebbe comunque dare particolari risultati,
+##perché le probabilità tendono quasi sempre a 0 o 1, e per evitare la maledizione della dimensionalità.
+## Pertanto, l'algoritmo di ML servirà solo per bilanciare titolo e contenuto.
 prepare.dataset <- function(data,
                             predictor.headlines,
                             predictor.contents) {
@@ -148,7 +147,7 @@ prepare.dataset <- function(data,
          content = predict.vector(predictor.contents,
                                   as.character(data$content)))
 }
-## prepara le matrici per l'algoritmo
+## Prepara le matrici per l'algoritmo
 test.set <- prepare.dataset(db.test,
                             headlines.predictor,
                             content.predictor)
@@ -161,8 +160,7 @@ train.set <- prepare.dataset(db.train,
 write.csv(train.set,
           file = "/Users/riccardocervero/Desktop/trainset.csv")
 
-## Modello Recurive Partitioning, 
-## modello ricorsivo ad albero decisionale 
+## Modello Recurive Partitioning, modello ricorsivo ad albero decisionale 
 ##che ha un maggior rischio di overfitting rispetto a RF (73.92431%)
 model.rpart <- train(section ~ publication + title + content,
                   data = train.set,
@@ -181,10 +179,8 @@ model.c <- train(section ~ publication + title + content,
                   method = "C5.0",
                   na.action = na.omit)
 
-## Modello SVM (84.41334%)
+## Modello SVM lineare per classificazione multiclasse (84.41334%)
 ## http://www.cs.cornell.edu/people/tj/publications/joachims_98a.pdf
-## https://www.hvitfeldt.me/blog/binary-text-classification-with-tidytext-and-caret/#svm
-# È un SVM lineare per classificazione multiclasse 
 model.svm <- train(section ~ publication + title + content,
                    data = train.set,
                    method = "svmLinear3",
@@ -196,7 +192,7 @@ model.lb <- train(section ~ publication + title + content,
                    method = "LogitBoost",
                    na.action = na.omit)
 
-## mostra i risultati
+## Estrazione dei risultati
 test.set$results.rpart <- predict(model.rpart, newdata = test.set)
 test.set$results.rf <- predict(model.rf, newdata = test.set)
 test.set$results.c <- predict(model.c, newdata = test.set)
@@ -209,7 +205,7 @@ mean(test.set$results.c == test.set$section)
 mean(test.set$results.svm == test.set$section)
 mean(test.set$results.lb == test.set$section,na.rm=TRUE)
 
-##Cross Validation con resampling stratificato
+## 3-Cross Validation con resampling stratificato in base alla sola variabile target 'section'
 cv <- function(db,iter=3,method) {
   i=1
   while (i<iter+1) {
@@ -249,9 +245,13 @@ cv <- function(db,iter=3,method) {
 }
 
 rpart = cv(db,method = "rpart")
+## Il risultato medio è 61.34793%, contro un 73.92431% ottenuto con doppia stratificazione.
 rf = cv(db,method = "rf")
-c05 = cv(db=db,method = "C5.0")
-svm = cv(db=db,method = "svmLinear3")
-lb = cv(db=db,method = "LogitBoost")
-
+c05 = cv(db,method = "C5.0")
+## Il risultato medio è 72.90707%, contro un 84.74166% ottenuto con doppia stratificazione.
+svm = cv(db,method = "svmLinear3")
+lb = cv(db,method = "LogitBoost")
+## Il risultato medio è 76.94287%, contro un 86.11929% ottenuto con doppia stratificazione,
+## miglior risultato.
+  
 save.image(file = "/Users/riccardocervero/Desktop/completed.RData")
